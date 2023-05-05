ca56f23fbe9453d0108b40ee4d0e041b4c36bd42
araiwa <naomi.araiwa@googlemail.com>
2023-05-05 14:51:21 +0200

    Make it your own

--- Flashcards (Kotlin)/task/src/flashcards/Main.kt
+++ Flashcards (Kotlin)/task/src/flashcards/Main.kt
@@ -1,23 +1,40 @@
 package flashcards
-val box = mutableListOf<Flashcard>()
 
-data class Flashcard(val term: String, val definition: String)
+val box = mutableMapOf<String, String>()
+
 fun main() {
     println("Input the number of cards:")
     val numberOfCards: Int = readln().toInt()
     repeat(numberOfCards) { index ->
-            println("Card #${index+1}:")
-            val term: String = readln()
-            println("The definition for card #${index+1}:")
-            val definition: String = readln()
-            box.add(Flashcard(term, definition))
+        println("Card #${index + 1}:")
+        var term: String = readln()
+        while (box.containsKey(term)) {
+            println("The term \"$term\" already exists. Try again:")
+            term = readln()
+        }
+        println("The definition for card #${index + 1}:")
+        var definition: String = readln()
+        while (box.containsValue(definition)) {
+            println("The definition \"$definition\" already exists. Try again:")
+            definition = readln()
+        }
+        box.put(term, definition)
 
     }
-    for (f in box){
-        println("Print the definition of \"${f.term}\":")
-        when(readln()){
-            f.definition -> println("Correct!")
-            else -> println("Wrong. The right answer is \"${f.definition}\".")
+    val rMap = box.entries.associate { (k,v)-> v to k }
+    for ((k, v) in box) {
+        println("Print the definition of \"${k}\":")
+        val input = readln()
+        when (input) {
+            v -> println("Correct!")
+            else -> {
+                print("Wrong. The right answer is \"${v}\"")
+                if(rMap.containsKey(input)){
+                    print("but your definition is correct for \"${rMap[input]}\"")
+                }
+                println(".")
+
+            }
         }
     }
 
