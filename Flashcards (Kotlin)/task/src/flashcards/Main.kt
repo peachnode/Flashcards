02c3c52bb21348c894b7f79eb19fd74844a28cf4
araiwa <naomi.araiwa@googlemail.com>
2023-05-05 13:53:07 +0200

    Make it your own

--- Flashcards (Kotlin)/task/src/flashcards/Main.kt
+++ Flashcards (Kotlin)/task/src/flashcards/Main.kt
@@ -1,11 +1,24 @@
 package flashcards
+val box = mutableListOf<Flashcard>()
 
+data class Flashcard(val term: String, val definition: String)
 fun main() {
-    val term: String = readln()
-    val definition: String = readln()
-    if(readln()==definition){
-        println("Your answer is right!")
-    }else{
-        println("Your answer is wrong...")
+    println("Input the number of cards:")
+    val numberOfCards: Int = readln().toInt()
+    repeat(numberOfCards) { index ->
+            println("Card #${index+1}:")
+            val term: String = readln()
+            println("The definition for card #${index+1}:")
+            val definition: String = readln()
+            box.add(Flashcard(term, definition))
+
     }
+    for (f in box){
+        println("Print the definition of \"${f.term}\":")
+        when(readln()){
+            f.definition -> println("Correct!")
+            else -> println("Wrong. The right answer is \"${f.definition}\".")
+        }
+    }
+
 }
