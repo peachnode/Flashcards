e41c2ad0f96031d0761ecbf6aa195eabd1e249f1
araiwa <naomi.araiwa@googlemail.com>
2023-05-05 18:37:32 +0200

    Menu, please!

--- Flashcards (Kotlin)/task/src/flashcards/Main.kt
+++ Flashcards (Kotlin)/task/src/flashcards/Main.kt
@@ -1,41 +1,102 @@
 package flashcards
 
+import java.io.File
+
+
 val box = mutableMapOf<String, String>()
-
-fun main() {
-    println("Input the number of cards:")
-    val numberOfCards: Int = readln().toInt()
-    repeat(numberOfCards) { index ->
-        println("Card #${index + 1}:")
-        var term: String = readln()
-        while (box.containsKey(term)) {
-            println("The term \"$term\" already exists. Try again:")
-            term = readln()
+fun add(){
+        println("The card:")
+        val term: String = readln()
+        if(box.containsKey(term)) {
+            println("The card \"$term\" already exists.")
+            return
         }
-        println("The definition for card #${index + 1}:")
-        var definition: String = readln()
-        while (box.containsValue(definition)) {
-            println("The definition \"$definition\" already exists. Try again:")
-            definition = readln()
+        println("The definition of the card:")
+        val definition: String = readln()
+        if (box.containsValue(definition)) {
+            println("The definition \"$definition\" already exists.")
+            return
         }
-        box.put(term, definition)
-
-    }
+    box[term] = definition
+    println("The pair (\"$term\":\"$definition\") has been added.")
+}
+fun ask(){
+    println("How many times to ask?")
+    val times: Int = readln().toInt()
     val rMap = box.entries.associate { (k,v)-> v to k }
-    for ((k, v) in box) {
-        println("Print the definition of \"${k}\":")
-        val input = readln()
-        when (input) {
-            v -> println("Correct!")
+    val entries =box.keys.toList().shuffled()
+    for ((index, key) in entries.withIndex()){
+        if(index > times){
+            break
+        }
+        val value: String? = box[key]
+        println("Print the definition of \"${key}\":")
+        when (val input = readln()) {
+            value -> println("Correct!")
             else -> {
-                print("Wrong. The right answer is \"${v}\"")
-                if(rMap.containsKey(input)){
+                print("Wrong. The right answer is \"${value}\"")
+                if (rMap.containsKey(input)) {
                     print("but your definition is correct for \"${rMap[input]}\"")
                 }
                 println(".")
-
             }
         }
     }
+}
+fun remove(){
+    println("Which card?")
+    val term: String = readln()
+    if(box.containsKey(term)){
+        box.remove(term)
+        println("The card has been removed")
+    }else{
+        println("Can't remove \"$term\": there is no such card.")
+    }
+}
+fun export(){
+    println("File name:")
+    val name: String = readln()
+    File(name).delete()
+    for ((k,v) in box){
+        File(name).appendText("$k;$v\n")
+    }
+    println("${box.size} cards have been saved.")
+}
+
+fun import(){
+    println("File name:")
+    val file: String = readln()
+    try{
+        val lines = File(file).readLines()
+        val importBox = mutableMapOf<String, String>()
+        for(line in lines){
+            println(line)
+            val (term, definition) = line.split(';')
+            importBox.put(term, definition)
+        }
+        for ((k,v) in importBox){
+            box.merge(k, v){oldval, newval -> newval}
+        }
+        println("${importBox.size} cards have been loaded.")
+    }catch(e: Exception){
+        println("File not found.")
+    }
+
+
+
+}
+fun main() {
+    while(true){
+        println("Input the action (add, remove, import, export, ask, exit):")
+        when(readln()){
+            "add" -> add()
+            "remove" -> remove()
+            "import" -> import()
+            "export" -> export()
+            "ask" -> ask()
+            "exit" -> break
+        }
+    }
+    println("Bye bye!")
 
 }
---
+++ Topics/The Math library/Fourth root/src/Main.kt
@@ -0,0 +1,7 @@
+import kotlin.math.*
+
+fun main() {
+    // put your code here
+    val input: Double = readln().toDouble()
+    println(Math.sqrt(Math.sqrt(input)))
+}
